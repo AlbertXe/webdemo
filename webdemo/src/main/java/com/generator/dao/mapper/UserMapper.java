a9f1c7377cc9a61e2d9066904b876424df1eb4ec
AlbertXe <563000409@qq.com>
2019-11-06 14:43:10 +0800

    mybatis 自动生成

---
+++ webdemo/src/main/java/com/generator/dao/mapper/UserMapper.java
@@ -0,0 +1,17 @@
+package com.generator.dao.mapper;
+
+import com.generator.dao.entity.User;
+
+public interface UserMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(User record);
+
+    int insertSelective(User record);
+
+    User selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(User record);
+
+    int updateByPrimaryKey(User record);
+}
