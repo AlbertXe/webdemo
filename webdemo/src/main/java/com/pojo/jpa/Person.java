3498bff2cf243606fdea0255d6f3bc207ada4f41
AlbertXe <563000409@qq.com>
2019-11-07 14:59:00 +0800

    spring jpa 创建表到数据库

---
+++ webdemo/src/main/java/com/pojo/jpa/Person.java
@@ -0,0 +1,19 @@
+package com.pojo.jpa;
+
+import javax.persistence.*;
+
+/**
+ * @author Administrator
+ * @date 2019-11-07 14:52
+ */
+@Entity
+@Table(name = "person")
+public class Person {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column
+    private String name;
+    @Column
+    private Integer age;
+}
