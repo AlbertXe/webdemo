8f0d21b7eb779e3fc79eb5d8e5356ddbf3dab99c
AlbertXe <563000409@qq.com>
2019-11-26 18:12:36 +0800

    监听器

---
+++ webdemo/src/main/java/com/listener/CleanUpListener.java
@@ -0,0 +1,23 @@
+package com.listener;
+
+import com.github.pagehelper.PageHelper;
+
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+
+/**
+ * @author AlbertXe
+ * @date 2019-11-26 16:25
+ */
+public class CleanUpListener implements ServletContextListener {
+
+    @Override
+    public void contextInitialized(ServletContextEvent sce) {
+    }
+
+    @Override
+    public void contextDestroyed(ServletContextEvent sce) {
+        PageHelper.clearPage();
+        System.out.println("调用Destroyed");
+    }
+}
---
+++ webdemo/src/main/java/com/listener/ListenerTestRegister.java
@@ -0,0 +1,22 @@
+package com.listener;
+
+import org.springframework.boot.web.servlet.ServletListenerRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.EventListener;
+
+/**
+ * 注冊器測試
+ * @author AlbertXe
+ * @date 2019-11-26 15:56
+ */
+@Configuration
+public class ListenerTestRegister {
+    @Bean
+    public ServletListenerRegistrationBean registerListener() {
+        ServletListenerRegistrationBean<EventListener> servletListenerRegistrationBean = new ServletListenerRegistrationBean<>(new CleanUpListener());
+        servletListenerRegistrationBean.setOrder(1);
+        return servletListenerRegistrationBean;
+    }
+}
