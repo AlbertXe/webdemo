97eeb7767f5f6426cf4c0f85436ad34c4d44cc2e
AlbertXe <563000409@qq.com>
2019-11-29 18:27:30 +0800

    leetcode 218

---
+++ webdemo/src/main/java/com/all/leetcode/linesweep/Demo218.java
@@ -0,0 +1,28 @@
+package com.all.leetcode.linesweep;
+
+import com.google.common.collect.Multiset;
+import javafx.util.Pair;
+import org.junit.Test;
+
+import java.util.Vector;
+
+/**
+ * 天际线问题
+ * @author AlbertXe
+ * @date 2019-11-29 17:55
+ */
+public class Demo218 {
+    @Test
+    public void test(){
+        //[2,9,10] 拆成 [2,-10]表示左边,[2,10]表示右端点
+        Vector<Vector<Integer>> buildings = new Vector(new Vector());
+        Vector<Pair> h = new Vector<>();
+        Multiset m;
+        Vector<Vector> res = new Vector<>(); // 结果
+
+        for (Vector<Integer> building : buildings) {
+
+        }
+
+    }
+}
