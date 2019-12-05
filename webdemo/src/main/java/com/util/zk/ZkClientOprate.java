59bbc8b52a03d6ac94c3423eddd40601a7e350e5
AlbertXe <563000409@qq.com>
2019-12-06 00:12:36 +0800

    zookeeper

---
+++ webdemo/src/main/java/com/util/zk/ZkClientOprate.java
@@ -0,0 +1,15 @@
+package com.util.zk;
+
+/**
+ * zkClient
+ * AlbertXe
+ * 2019/12/5 23:56
+ */
+public class ZkClientOprate {
+    private static final String server = "192.168.199.100:2181";
+    private static int timeOut = 30000;
+
+    public static void main(String[] args) {
+//        ZkClient client = new ZkClient(server, timeOut);
+    }
+}
