935d56f3d5f0679b160e3279eadd6200ef448d5f
AlbertXe <563000409@qq.com>
2019-12-10 19:31:15 +0800

    日志更改，需要排除自带的日志

--- webdemo/src/main/java/com/controller/UserController.java
+++ webdemo/src/main/java/com/controller/UserController.java
@@ -54,6 +54,7 @@ public class UserController {
 
     @RequestMapping("/login")
     public void login() {
+
     }
 
 
--- webdemo/src/main/java/com/util/ContextUtils.java
+++ webdemo/src/main/java/com/util/ContextUtils.java
@@ -1,14 +1,22 @@
 package com.util;
 
-import org.springframework.web.context.support.WebApplicationContextUtils;
+import org.springframework.beans.BeansException;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
 
 /**
  * @author AlbertXe
  * @date 2019-12-07 16:29
  */
-public class ContextUtils extends WebApplicationContextUtils {
-    public ContextUtils() {
-        super();
+public class ContextUtils implements ApplicationContextAware {
+    private static ApplicationContext context;
+
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        ContextUtils.context = applicationContext;
     }
 
+    public static ApplicationContext getContext() {
+        return context;
+    }
 }
