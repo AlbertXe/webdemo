e4b32f01a2ad04cc656b6ea1ac706541a18a7f20
AlbertXe <563000409@qq.com>
2019-12-11 15:06:11 +0800

    consul

---
+++ webdemo/src/main/java/com/service/ConsulService.java
@@ -0,0 +1,22 @@
+package com.service;
+
+import org.springframework.stereotype.Service;
+
+/**
+ * @author AlbertXe
+ * @date 2019-12-11 11:15
+ */
+@Service
+public class ConsulService {
+
+    public void registerService(String serviceName, String serviceId) {
+//        Consul consul = Consul.builder().build();
+//        AgentClient client = consul.agentClient();
+//        URI uri =  URI.create("http://localhost:8080/health");
+//        try {
+//            client.register(8080,uri.toURL(), 3, serviceName, serviceName, "dev");
+//        } catch (MalformedURLException e) {
+//            e.printStackTrace();
+//        }
+    }
+}
