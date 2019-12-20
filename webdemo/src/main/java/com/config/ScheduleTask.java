182862e2a68b614f270dba0e06a4aa012984bcc7
AlbertXe <563000409@qq.com>
2019-12-20 14:32:30 +0800

    springboot的定时任务

---
+++ webdemo/src/main/java/com/config/ScheduleTask.java
@@ -0,0 +1,19 @@
+package com.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
+
+/**
+ * @author AlbertXe
+ * @date 2019-12-19 19:31
+ */
+@Configuration
+@EnableScheduling
+public class ScheduleTask {
+
+    @Scheduled(cron = "0/5 * * * * ?")
+    public void task1() {
+        System.out.println("执行静态定时任务1");
+    }
+}
