8f661790865ec3410afdffb49f882eeb469fa61e
AlbertXe <563000409@qq.com>
2020-04-17 17:42:13 +0800

    大文件去重

---
+++ webdemo/src/test/java/com/util/largefilesort/DistinctFile.java
@@ -0,0 +1,17 @@
+package com.util.largefilesort;
+
+import java.io.File;
+
+/**
+ * 对大文件去重
+ * 1. 每行hash取模, 相同的放入不同的文件
+ * 2. 对小文件去重合并
+ */
+public class DistinctFile {
+
+    public void distinct(File file) {
+
+
+    }
+
+}
