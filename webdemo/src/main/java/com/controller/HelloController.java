03402f9a3b8b94c4cd9ef8810c1412b7bb8369ef
AlbertXe <563000409@qq.com>
2020-05-30 16:24:07 +0800

    html

---
+++ webdemo/src/main/java/com/controller/HelloController.java
@@ -0,0 +1,26 @@
+package com.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Controller
+public class HelloController {
+
+    @RequestMapping("/hello")
+    public String hello(HttpServletRequest request,
+                        @RequestParam(value = "name", required = false, defaultValue = "albertXe") String name) {
+        request.setAttribute("name", name);
+        return "hello";
+    }
+
+    @RequestMapping("/index")
+    public String index(HttpServletRequest request,
+                        @RequestParam(value = "name", required = false, defaultValue = "albertXe") String name) {
+        request.setAttribute("name", name);
+        return "index";
+    }
+
+}
